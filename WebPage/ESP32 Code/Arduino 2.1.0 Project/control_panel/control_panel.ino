95a28f84bd5fd7a307693ad46fb6e66c6dd5fb7a
Dan Heeks <danheeks@gmail.com>
2025-03-20 15:24:54 +0000

    OK. I have all the basics working; Modbus, Wifi with web page, LEDs, SPIFFS. :)  So now to get it to do a sequence that we want.

--- WebPage/ESP32 Code/Arduino 2.1.0 Project/control_panel/control_panel.ino
+++ WebPage/ESP32 Code/Arduino 2.1.0 Project/control_panel/control_panel.ino
@@ -1,5 +1,5 @@
 #include <WiFi.h>
-#include <ESPAsyncWebServer.h>
+#include <WebServer.h>
 #include <FS.h>
 #include <SPIFFS.h>
 #include <ArduinoJson.h>
@@ -41,38 +41,17 @@
 #define STATUS_OK 0
 #define STATUS_MODBUS_ERROR 1
 
-// type these letters, then Enter to do:
-// o - move to open position
-// c - move to closed position
-// g50 - move to 50% open (for example)
-// s - stop move
-// b - print battery voltage
-// S - print status
-// w - print working angle
-// p - print position (as a percentage)
-// d - print all input registers
-// O - print 1 if open relay is activated, else 0
-// C - print 1 if closed relay is activated, else 0
-// m - print 1 if actuator is moving, else 0
-// t - print 1 if failed to complete a move, else 0
-// l - print 1 if battery to low to start a move, else 0
-// D - print all discrete inputs
-
-
 // Set your WiFi credentials
 const char* ssid = "BartonFirtop-Control";
 const char* password = "123456789";
 uint16_t angle = 0;
 uint8_t status = STATUS_OK;
 uint8_t modbus_error = 0;
-
-unsigned long myTime;
-
-myTime = millis();
+unsigned long oldTime;
 
 // Create an instance of the web server
 #ifdef USE_WIFI
-AsyncWebServer server(80);
+WebServer server(80);
 #endif
 
 #ifdef USE_MODBUS
@@ -139,15 +118,15 @@ void handleStop() {
     stopActuator();
 }
 
-void handleBatteryVoltage(AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", String(14.0));
+void handleBatteryVoltage() {
+    server.send(200, "text/plain", String(14.0));
 }
 
-void handleAngle(AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", String(angle));
+void handleAngle() {
+    server.send(200, "text/plain", String(angle));
 }
 
-void handleStatus(AsyncWebServerRequest *request) {
+void handleStatus() {
   String status_str = "OK";
   switch(status)
   {
@@ -155,7 +134,7 @@ void handleStatus(AsyncWebServerRequest *request) {
       status_str = String("Modbus Error: ") + String(modbus_error);
       break;
   }
-    request->send(200, "text/plain", status_str);
+    server.send(200, "text/plain", status_str);
 }
 
 void printInputRegister(uint16_t u16ReadAddress, char* str)
@@ -217,12 +196,133 @@ void printDiscreteInput(uint16_t u16ReadAddress, char* str)
 #endif
 }
 
+const char* htmlPage = R"rawliteral(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Barton Firtop Dashboard</title>
+    <style>
+        /* Add your styles here */
+    </style>
+</head>
+<body>
+    <button class="open-a" onclick="sendRequest('openA')">Open A</button>
+    <button class="auto" onclick="sendRequest('auto')">Auto</button>
+    <button class="open-b" onclick="sendRequest('openB')">Open B</button>
+    <button class="stop" onclick="sendRequest('stop')">STOP</button>
+
+    <script>
+        function sendRequest(action) {
+            fetch('/action', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ action: action })
+            })
+            .then(response => response.text())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+        }
+    </script>
+</body>
+</html>
+)rawliteral";
+
+void handleRoot() {
+    File file = SPIFFS.open("/index.html", "r");
+    if (!file) {
+        server.send(500, "text/plain", "Failed to open file");
+        return;
+    }
+    server.streamFile(file, "text/html");
+    file.close();
+}
+
+// Serve static files (CSS, JS, images)
+void handleFileRequest() {
+    String path = server.uri();
+    Serial.println("Requested file: " + path);
+
+    if (path == "/") {
+        path = "/index.html";  // Default to index.html
+    }
+
+    File file = SPIFFS.open(path, "r");
+    if (!file) {
+        server.send(404, "text/plain", "File Not Found");
+        return;
+    }
+
+    // Determine content type
+    String contentType = "text/plain";
+    if (path.endsWith(".html")) contentType = "text/html";
+    else if (path.endsWith(".css")) contentType = "text/css";
+    else if (path.endsWith(".js")) contentType = "application/javascript";
+    else if (path.endsWith(".png")) contentType = "image/png";
+    else if (path.endsWith(".jpg") || path.endsWith(".jpeg")) contentType = "image/jpeg";
+    else if (path.endsWith(".gif")) contentType = "image/gif";
+    else if (path.endsWith(".ico")) contentType = "image/x-icon";
+    else if (path.endsWith(".svg")) contentType = "image/svg+xml";
+
+    server.streamFile(file, contentType);
+    file.close();
+}
+
+void handlePostAction() {
+    if (server.method() != HTTP_POST) {
+        server.send(405, "text/plain", "Method Not Allowed");
+        return;
+    }
+
+    // Read the request body
+    String body = server.arg(0);
+    Serial.println("Received POST body: " + body);
+
+    // Parse JSON
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, body);
+    if (error) {
+        Serial.println("Failed to parse JSON");
+        server.send(400, "text/plain", "Bad Request: Invalid JSON");
+        return;
+    }
+
+    String action = doc["action"];
+    Serial.println("Action: " + action);
+
+    // Perform actions based on the "action" field
+    if (action == "openA") {
+        Serial.println("Opening Chamber A...");
+        handleOpenA();
+    } else if (action == "openB") {
+        Serial.println("Opening Chamber B...");
+        handleOpenB();
+    } else if (action == "stop") {
+        Serial.println("Stopping system...");
+        handleStop();
+    } else {
+        Serial.println("Unknown action received");
+        server.send(400, "text/plain", "Bad Request: Unknown action");
+        return;
+    }
+
+    server.send(200, "text/plain", "Action received: " + action);
+}
+
+// Handle 404 (Not Found)
+void handleNotFound() {
+    server.send(404, "text/plain", "404: Not Found");
+}
+
 void setup() {
 #ifdef USE_WATCHDOG
-    rtc_wdt_protect_off();    // Turns off the automatic wdt service
-rtc_wdt_enable();         // Turn it on manually
-rtc_wdt_set_time(RTC_WDT_STAGE0, 5000);  // Define how long you desire to let dog wait.
+  rtc_wdt_protect_off();    // Turns off the automatic wdt service
+  rtc_wdt_enable();         // Turn it on manually
+  rtc_wdt_set_time(RTC_WDT_STAGE0, 5000);  // Define how long you desire to let dog wait.
 #endif
+
+  oldTime = millis();
 
 #ifdef USE_LEDS
   pinMode(LED1, OUTPUT);
@@ -266,52 +366,13 @@ rtc_wdt_set_time(RTC_WDT_STAGE0, 5000);  // Define how long you desire to let do
     }
 
 #ifdef USE_WIFI
-    // Serve static files from SPIFFS
-    server.serveStatic("/", SPIFFS, "/").setDefaultFile("index.html");
 
-      // Handle POST requests to /action
-    server.on("/action", HTTP_POST, [](AsyncWebServerRequest *request) {}, 
-        NULL,  // No body handler for multipart
-        [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-            String body = String((char*)data).substring(0, len);
-            Serial.println("Received POST body: " + body);
-
-            // Parse JSON
-            StaticJsonDocument<200> doc;
-            DeserializationError error = deserializeJson(doc, body);
-            if (error) {
-                Serial.println("Failed to parse JSON");
-                request->send(400, "text/plain", "Bad Request: Invalid JSON");
-                return;
-            }
-
-            String action = doc["action"]; // Extract the "action" field
-            Serial.println("Action: " + action);
-
-            // Perform actions based on the "action" field
-            if (action == "openA") {
-                Serial.println("Opening Chamber A...");
-                // Add your code to open Chamber A
-                handleOpenA();
-            } else if (action == "openB") {
-                Serial.println("Opening Chamber B...");
-                // Add your code to open Chamber B
-                handleOpenB();
-            } else if (action == "stop") {
-                Serial.println("Stopping system...");
-                // Add your code to stop the system
-                handleStop();
-            } else {
-                Serial.println("Unknown action received");
-            }
-
-            request->send(200, "text/plain", "Action received: " + action);
-        }
-    );
-
-    server.on("/battery-voltage", HTTP_GET, handleBatteryVoltage);
+    server.on("/", HTTP_GET, handleRoot);
+     server.on("/action", HTTP_POST, handlePostAction);
+   server.on("/battery-voltage", HTTP_GET, handleBatteryVoltage);
     server.on("/angle", HTTP_GET, handleAngle);
     server.on("/status", HTTP_GET, handleStatus);
+    server.onNotFound(handleFileRequest);  // Serve static files from SPIFFS
 
     // Start the server
     server.begin();
@@ -386,187 +447,9 @@ void SetLEDs(uint16_t value)
 #endif
 }
 
-void loop() {
-  uint8_t result;
-  uint16_t data;
-  
-  //Serial.println("transmit low...");
-  // set word 0 of TX buffer to least-significant word of counter (bits 15..0)
-  //node.setTransmitBuffer(0, lowWord(i));
-  
-  //Serial.println("transmit high...");
-  // set word 1 of TX buffer to most-significant word of counter (bits 31..16)
-  //node.setTransmitBuffer(1, highWord(i));
-  
-  //Serial.println("write...");
-  // slave: write TX buffer to (2) 16-bit registers starting at register 0
-  //result = node.writeMultipleRegisters(0, 2);
-
-  while (Serial.available() > 0) {
-    // cjheck serial from console
-
-    char c = Serial.read();
-    if(reading_number != 0)
-    {
-    switch(c)
-    {
-    case '0':
-    case '1':
-    case '2':
-    case '3':
-    case '4':
-    case '5':
-    case '6':
-    case '7':
-    case '8':
-    case '9':
-    case '.':
-    case '-':
-      num[i] = c;
-      i++;
-      break;
-      default:
-      num[i] = 0;
-      if(reading_number == 'g')
-      {
-    uint16_t percentage = atoi(num);
-    i = 0;
-#ifdef USE_MODBUS  
-    node.writeSingleRegister(0,percentage);
-#endif
-    Serial.print("go to percentage: ");
-    Serial.println(percentage);
-      }
-      else if(reading_number == 'L')
-      {
-        SetLEDs(atoi(num));
-      }
-      reading_number = 0;
-
-    delay(100);
-    
-#ifdef USE_MODBUS  
-      // tell actuator to move to that percentage
-      node.writeSingleCoil(2, 1);
-#endif      
-      break;
-    }
-    }
-    else
-    { // not reading_number
-    if (c == 'o') {
-      // tell actuator to open
-#ifdef USE_MODBUS  
-      node.writeSingleCoil(0, 1);
-#endif
-    }
-    if (c == 'c') {
-      // tell actuator to close
-#ifdef USE_MODBUS  
-      node.writeSingleCoil(1, 1);
-#endif
-    }
-    if(c == 'g')
-    {
-      reading_number = 'g';
-   }
-    if (c == 's') {
-      // tell actuator to stop
-#ifdef USE_MODBUS  
-      node.writeSingleCoil(3, 1);
-#endif
-    }
-    if (c == 'b') {
-        printInputRegister(0, "  battery = ");
-    }
-    if (c == 'p') {
-        printInputRegister(1, "  position = ");
-    }
-    if (c == 'S') {
-        printInputRegister(2, "  status = ");
-    }
-    if (c == 'w') {
-        printInputRegister(3, "  working angle = ");
-    }
-    if (c == 'd') {
-#ifdef USE_MODBUS  
-        result = node.readInputRegisters(0, 4);
-        
-        // do something with data if read is successful
-        if (result == node.ku8MBSuccess)
-        {
-            Serial.print("  battery = ");
-            float battery = 0.001 * (float)((uint16_t)(node.getResponseBuffer(0)));
-            Serial.println(battery);
-            Serial.print("  position = ");
-            Serial.println(node.getResponseBuffer(1));
-            Serial.print("  status = ");
-            Serial.println((uint16_t)(node.getResponseBuffer(2)));
-            Serial.print("  working angle = ");
-            Serial.println((uint16_t)(node.getResponseBuffer(3)));
-        }
-        else
-        {
-          Serial.print("Error code: ");
-          Serial.println(result);
-        }
-#endif
-    }
-#ifdef USE_MODBUS
-    if (c == 'O') printDiscreteInput(0, "Open Relay = ");
-    if (c == 'C') printDiscreteInput(1, "Closed Relay = ");
-    if (c == 'm') printDiscreteInput(2, "moving = ");
-    if (c == 't') printDiscreteInput(3, "failed to complete = ");
-    if (c == 'l') printDiscreteInput(4, "too low to move = ");
-#endif
-    if (c == 'L')
-          reading_number = 'L';
-
-    if (c == 'D') {
-#ifdef USE_MODBUS
-        result = node.readDiscreteInputs(0, 5);
-        
-        // do something with data if read is successful
-        if (result == node.ku8MBSuccess)
-        {
-            uint8_t result = node.getResponseBuffer(0);
-            Serial.print("Open Relay = ");
-            Serial.println((int)((result & 1) != 0));
-            Serial.print("Closed Relay = ");
-            Serial.println((int)((result & 2) != 0));
-            Serial.print("moving = ");
-            Serial.println((int)((result & 4) != 0));
-            Serial.print("failed to complete = ");
-            Serial.println((int)((result & 8) != 0));
-            Serial.print("too low to move = ");
-            Serial.println((int)((result & 16) != 0));
-       }
-      else
-      {
-        Serial.print("Error code: ");
-        Serial.println(result);
-      }
-#endif
-    }
-
-    }
-  }
-
-  angle = getInputRegister(1);
-if(modbus_error == 0)
+void LEDTest()
 {
-    Serial.print("Modbus Success Angle = ");
-    Serial.println(angle);
-}  
-  status = STATUS_OK;
-  if(modbus_error != 0)
-  {
-    status = STATUS_MODBUS_ERROR;
-  }
-
 #ifdef USE_LEDS
-if(delay_count == 0)
-{
   uint16_t led_value = 0x0001;
   for(int i = 0; i<16;i++)
   {
@@ -579,12 +462,34 @@ if(delay_count == 0)
   led_test_count++;
   Serial.print("LED test ");
   Serial.println(led_test_count);
-}
 #endif
+}
 
-  delay(2000);
-  delay_count++;
-  if(delay_count == 30)delay_count = 0;
+void loop() {
+  server.handleClient();  // Handle incoming requests
+
+  unsigned long newTime = millis();
+  if(newTime > oldTime + 2000)
+  {
+    oldTime = newTime;
+    angle = getInputRegister(1);
+    if(modbus_error == 0)
+    {
+      Serial.print("Modbus Success Angle = ");
+      Serial.println(angle);
+      status = STATUS_OK;
+    }  
+    else
+    {
+      status = STATUS_MODBUS_ERROR;
+    }
+
+    if(delay_count == 0)
+      LEDTest();
+    delay_count++;
+    if(delay_count >= 30)
+      delay_count = 0;
+  }
 
 #ifdef USE_WATCHDOG
    // Reset the watchdog timer
